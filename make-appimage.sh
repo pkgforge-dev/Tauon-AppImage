2590f77c35fc5387382870e13523d928c817d4af
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-19 16:10:39 -0400

    fix several issues (#1)
    * fix readonly bug and make own launcher script
    
    * no longer needed
    
    * add missing lib
    
    * bump `LATEST_VERSION` [skip ci]
    
    * add another missing lib
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,20 +3,27 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q tauon-music-box-git | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q tauon-music-box | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/tauonmb.svg
 export DESKTOP=/usr/share/applications/tauonmb.desktop
-
 export DEPLOY_SYS_PYTHON=1
 export DEPLOY_PIPEWIRE=1
 export DEPLOY_SDL=1
 
+# fix bug making the app assume that it can write to its site-package dir
+find /usr/lib/python*/site-packages \
+	-type f -name '*.py' -exec sed -i -e 's|/snap/|/|g' {} \;
+
 # Deploy dependencies
-quick-sharun /usr/bin/tauonmb /usr/bin/tauon /usr/lib/libgtk-3.so*
+quick-sharun \
+	/usr/bin/tauonmb           \
+	/usr/lib/libgtk-3.so*      \
+	/usr/lib/libsamplerate.so* \
+	/usr/lib/libkissfft-float.so*
 
 # Additional changes can be done in between here
 
