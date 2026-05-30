ad21875cf1399fbe540f1438fbe17c5fce6c2776
domesya <163451587+domesya@users.noreply.github.com>
2026-05-30 17:16:20 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,14 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm tauon-music-box pipewire-audio pipewire-jack p7zip unrar opusfile mpg123 flac libvorbis wavpack libgme libnotify python-jxlpy python-pypresence python-tekore python-plexapi python-pychromecast python-tidalapi
+pacman -Syu --noconfirm \
+  tauon-music-box \
+  pipewire-audio pipewire-jack \
+  p7zip unrar \
+  opusfile mpg123 flac libvorbis wavpack \
+  libgme libnotify \
+  python-jxlpy python-pypresence python-tekore python-plexapi python-pychromecast python-tidalapi \
+  libimagequant
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
