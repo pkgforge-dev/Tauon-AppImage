5124e86e3ea5b701c5d8caad319dffcaa0f04387
Samueru-sama <xdglawyer@outlook.com>
2026-09-02 19:35:24 -0400

    build manually

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,27 +7,57 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	flac                \
-	libgme              \
-	libimagequant       \
-	libnotify           \
-	libvorbis           \
-	mpg123              \
-	openjpeg2           \
-	opusfile            \
-	p7zip               \
-	pipewire-audio      \
-	pipewire-jack       \
-	python-jxlpy        \
-	python-opengl       \
-	python-plexapi      \
-	python-pychromecast \
-	python-pypresence   \
-	python-tekore       \
-	python-tidalapi     \
-	tauon-music-box     \
-	unrar               \
-	wavpack
+	cargo                   \
+	flac                    \
+	kissfft                 \
+	libappindicator         \
+	libgme                  \
+	libimagequant           \
+	libnotify               \
+	libopenmpt              \
+	libprojectm             \
+	libsamplerate           \
+	libvorbis               \
+	miniaudio               \
+	mpg123                  \
+	noto-fonts              \
+	noto-fonts-extra        \
+	opencc                  \
+	openjpeg2               \
+	opusfile                \
+	p7zip                   \
+	pipewire-audio          \
+	pipewire-jack           \
+	pkgconf                 \
+	python-beautifulsoup4   \
+	python-build            \
+	python-cairo            \
+	python-dbus             \
+	python-gobject          \
+	python-installer        \
+	python-jxlpy            \
+	python-musicbrainzngs   \
+	python-mutagen          \
+	python-natsort          \
+	python-opengl           \
+	python-pillow           \
+	python-plexapi          \
+	python-pychromecast     \
+	python-pylast           \
+	python-pypresence       \
+	python-pysdl3           \
+	python-rapidfuzz        \
+	python-requests         \
+	python-send2trash       \
+	python-setproctitle     \
+	python-tekore           \
+	python-tidalapi         \
+	python-unidecode        \
+	python-websocket-client \
+	sdl3_image              \
+	unrar                   \
+	wavpack                 \
+	xdg-utils
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -36,12 +66,46 @@ get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 # Comment this out if you need an AUR package
 #make-aur-package tauon-music-box-git
 
-# If the application needs to be manually built that has to be done down here
+echo "Building tauon-music-box from source..."
+echo "---------------------------------------------------------------"
+git clone https://github.com/Taiko2k/Tauon.git ./Tauon && (
+	cd ./Tauon
 
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
+	git fetch --tags origin
+	TAG=$(git tag --sort=-v:refname | grep -vi 'pre\|rc\|alpha\|beta' | head -1)
+	git checkout "$TAG"
+	echo "${TAG#v}" > ~/version
+
+	# use system kissfft instead of the expected cloned repository
+	sed -i 's|"src/phazor/kissfft/kiss_fftr.c", "src/phazor/kissfft/kiss_fft.c", ||g' pyproject.toml
+	sed -i 's|"samplerate"|"kissfft-float", "samplerate"|g' pyproject.toml
+	sed -i 's|com.github.taiko2k.tauonmb.desktop|tauonmb.desktop|' extra/com.github.taiko2k.tauonmb.appdata.xml
+
+	python -m build --wheel
+
+	# Tiny Rust binary that calculates LRCLIB challenges
+	(
+		cd src/lrclib-solver
+		export RUSTUP_TOOLCHAIN=stable
+		export CARGO_TARGET_DIR=target
+		cargo fetch --locked --target host-tuple
+		CFLAGS="${CFLAGS-} -fno-lto" cargo build --frozen --release --all-features
+	)
+
+	python -m installer --destdir=/ dist/*.whl
+
+	for dir in src/tauon/locale/*; do
+		install -Dm644 \
+			"${dir}/LC_MESSAGES/"*.mo \
+			-t "/usr/share/locale/$(basename "${dir}")/LC_MESSAGES"
+	done
+
+	install -Dm644 extra/tauonmb.desktop -t /usr/share/applications
+	install -Dm644 extra/com.github.taiko2k.tauonmb.appdata.xml -t /usr/share/metainfo
+	install -Dm644 extra/tauonmb-symbolic.svg -t /usr/share/icons/hicolor/symbolic/apps
+	install -Dm644 extra/tauonmb.svg -t /usr/share/icons/hicolor/scalable/apps
+	install -Dm755 extra/tauonmb.sh /usr/bin/tauon
+
+	site_packages=$(python -c "import site; print(site.getsitepackages()[0])")
+	install -Dm755 src/lrclib-solver/target/release/lrclib-solver "${site_packages}/tauon/lrclib-solver"
+)
--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q tauon-music-box | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
@@ -27,7 +26,7 @@ quick-sharun \
 	/usr/lib/libgme.so*           \
 	/usr/lib/libwavpack.so*       \
 	/usr/lib/libnotify.so*        \
-	/usr/lib/libopenmpt.so*	      \
+	/usr/lib/libopenmpt.so*       \
 	/usr/lib/libvorbisfile.so*    \
 	/usr/lib/libopusfile.so*      \
 	/usr/lib/libsamplerate.so*    \
